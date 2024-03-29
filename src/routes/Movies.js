fa6115fd685e74941821b6e6e555b8a1f2c729a6
bookhabit <qnrgoqlt@naver.com>
2024-03-29 21:44:22 +0900

    Movies API 연동

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,7 +1,40 @@
+import { gql, useApolloClient } from "@apollo/client"
+import { useEffect, useState } from "react"
+
 export default function Movies(){
+    const client = useApolloClient()
+    const [movies,setMovies] = useState()
+
+    useEffect(()=>{
+        client.query({
+            query:gql`
+                {
+                    allMovies{
+                        title
+                    }
+                }
+            `
+        }).then(results=>{
+            console.log(results)
+            setMovies(results.data.allMovies)
+        })
+    },[client])
+
     return (
         <div>
-            This is a list of movies.
+            {!movies && 
+                <div>
+                    영화 데이터를 불러오지 못했습니다.
+                </div>
+            }
+            <div>
+                {movies?.map((movie)=>(
+                    <li key={movie.id}>
+                        {movie.title}
+                    </li>
+                    )
+                )}
+            </div>
         </div>
     )
 }
